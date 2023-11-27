3ba2586857cd30504d709e6bb83a6c29fe981f40
evef <eveforest@163.com>
2023-11-27 16:29:43 +0800

    init

---
+++ src/test/java/com/evefyou/base/core/JavaEvefyouBaseCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evefyou.base.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEvefyouBaseCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
